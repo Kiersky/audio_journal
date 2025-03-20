34eccdc51c0e9f7bad25b358dbb4aee79dda46a0
chris <krzysiek002@gmail.com>
2025-03-20 16:13:53 +0100

    correct typos

--- preload.js
+++ preload.js
@@ -1,11 +1,11 @@
 const { contextBridge, ipcRenderer } = require('electron');
 const log = require('electron-log');
 
-contextBridge.exposeInMainWorld('audio_journal', {
+contextBridge.exposeInMainWorld('audioJournal', {
 
-    startRecorording: () => ipcRenderer.invoke('start-recording'),
+    startRecording: () => ipcRenderer.invoke('start-recording'),
     stopRecording: () => ipcRenderer.invoke('stop-recording'),
-    playRecording: (id) => ipcRenderer.invoke('play-recording'),
+    playRecording: (id) => ipcRenderer.invoke('play-recording', id),
 
     onRecordingComplete: (callback) => {
         ipcRenderer.on('recording-complete', (event, data) => callback(data));
